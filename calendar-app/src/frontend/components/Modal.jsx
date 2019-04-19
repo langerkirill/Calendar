dae3fcd0e1420540ffafddffd2fd8675dd1976c8
Kirill <langerkirill@gmail.com>
2019-04-18 19:41:44 -0600

    added times to modal

--- calendar-app/src/frontend/components/Modal.jsx
+++ calendar-app/src/frontend/components/Modal.jsx
@@ -1,63 +1,77 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import '../styling/Modal.css'
+import { timesArray } from '../../utils/timesArray.js'
 
-const renderBody = ({ message, children }) => {
-    return message ?
-        <div dangerouslySetInnerHTML={{ __html: message }} />
-        :
-        children;
-};
+class Modal extends Component {
 
-const Modal = (props) => {
+    constructor(props){
+        super(props)
+        this.state = {
+            formText: '',
+            formTime: null
+        }
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
 
-    const {
-        id,
-        style,
-        handleClose,
-        handleConfirm,
-        title,
-    } = props
+    handleChange(e) {
+        const formText = e.target.value
+        this.setState({ formText })
+    }
 
-    return (
-        <div className='modal-container' id={id}>
-            <div className='modal-class' style={style}>
-                <div className='modal-header'>
-                    {title ? <div className='modal-title'>{title}</div> : null}
-                </div>
-                <div className='modal-body'>
-                    {renderBody(props)}
-                </div>
-                <div className='modal-form-container'>
-                    <form>
-                        <label> Event Name
-                            <input></input>
-                        </label>
-                        <br></br>
-                        <label> Time
-                            <select></select>
-                        </label>
-                    </form>
-                </div>
-                <div>
-                    <div className={'modal-buttons'}>
-                        <div>
-                            <button className='modal-button' onClick={handleConfirm}>Ok</button>
-                        </div>
-                        <div onClick={handleClose}>
-                            <button className='modal-button'>Close</button>
+    handleSubmit(e) {
+        e.preventDefault()
+        debugger
+    }
+
+    timeOptions() {
+        return timesArray.map((time, i) => <option key={i}>{time}</option>)
+    }
+
+    render() {
+        const {
+            id,
+            style,
+            handleClose,
+            handleConfirm,
+            title,
+        } = this.props
+
+        return (
+            <div className='modal-container' id={id}>
+                <div className='modal-class' style={style}>
+                    <div className='modal-header'>
+                        {title ? <div className='modal-title'>{title}</div> : null}
+                    </div>
+                    <div className='modal-form-container'>
+                        <form >
+                            <label> Event Name
+                                <input onChange={(e) => this.handleChange(e)} defaultValue={this.state.formText}></input>
+                            </label>
+                            <br></br>
+                            <label> Time
+                                <select>
+                                    {this.timeOptions()}
+                                </select>
+                            </label>
+                        </form>
+                    </div>
+                    <div>
+                        <div className={'modal-buttons'}>
+                            <div>
+                                <button className='modal-button' onClick={(e) => this.handleSubmit(e)}>Ok</button>
+                            </div>
+                            <div onClick={handleClose}>
+                                <button className='modal-button'>Close</button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
-
-};
-
-Modal.defaultProps = {
-    buttonText: 'OK'
-};
+        );
+    };
+}
 
 Modal.propTypes = {
     handleClose: PropTypes.func,
