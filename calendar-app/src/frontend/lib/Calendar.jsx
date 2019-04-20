52350dc1d717ac63dac94f305b3cf11cdc5bc98e
Kirill <langerkirill@gmail.com>
2019-04-19 21:47:04 -0600

    you can change the month

--- calendar-app/src/frontend/lib/Calendar.jsx
+++ calendar-app/src/frontend/lib/Calendar.jsx
@@ -1,111 +1,39 @@
-import React, { Component } from 'react'
-import '../styling/calendar.css'
-import Week from '../components/Week'
-import WeekDayNames from '../components/WeekDayNames'
+import React, { Component, Fragment } from 'react'
+import Month from './Month'
 import moment from 'moment'
 
-class Calendar extends Component {
+export default class Calendar extends Component {
 
-    constructor(props) {
+    constructor(props){
         super(props)
         this.state = {
-            dayArray: []
+            relativeMoment: null,
+            monthNumber: 1
         }
+        this.changeTheMonth = this.changeTheMonth.bind(this)
     }
 
-    loadTheDays(firstDay, dayArray, daysToFill, daysInLastMonth){
-        while (daysToFill > 0) {
-            dayArray.unshift(daysInLastMonth)
-            daysInLastMonth--
-            daysToFill--
-        }
-
-        let i = 1;
-        while (dayArray.length < 35) {
-            dayArray.push(i)
-            i++
-        }
-
-        return dayArray
+    changeTheMonth(direction) {
+        const monthNumber = direction === 'forward' 
+            ? this.state.monthNumber + 1 
+            : this.state.monthNumber - 1
+        const relativeMoment = moment().add(monthNumber, 'M')
+        debugger
+        this.setState({ relativeMoment, monthNumber })
     }
 
-    divideUpDays(firstDay, daysInMonth, daysInLastMonth, dayArray){
-        let daysToFill;
-        
-        switch (firstDay) {
-            case 'Monday':
-                daysToFill = 0;
-                break;
-            case 'Teusday':
-                daysToFill = 1;
-                break;
-            case 'Wednesday':
-                daysToFill = 2;
-                break;
-            case 'Thursday':
-                daysToFill = 3;
-                break;
-            case 'Friday':
-                daysToFill = 4;
-                break;
-            case 'Saturday':
-                daysToFill = 5;
-                break;
-            case 'Sunday':
-                daysToFill = 6;
-                break;
-            default:
-                daysToFill = null;
-                break;
-        }
-        return this.loadTheDays(firstDay, dayArray, daysToFill, daysInLastMonth)
-    }
-
-    getDayArray(daysInMonth){
-        const arrDays = []
-
-        while (daysInMonth) {
-            const current = daysInMonth
-            arrDays.unshift(current)
-            daysInMonth--
-        }
-        return arrDays
-    }
-
-    componentDidMount() {
-        //get days in the month information
-        const daysInMonth = moment().daysInMonth()
-        const daysInLastMonth = moment().subtract(1, 'month').daysInMonth()
-        const firstDay = moment().startOf('month').format('dddd')
-        const dayArray = this.getDayArray(daysInMonth)
-        this.divideUpDays(firstDay, daysInMonth, daysInLastMonth, dayArray)
-        this.setState({ dayArray })
+    componentDidMount(){
+        const relativeMoment = moment().add(1, 'M')
+        this.setState({ relativeMoment })
     }
 
     render() {
         return (
-            <div className="calendar-positioner">
-                <div className='calendar-container'>
-                    <table>
-                        <thead className='days-of-week'>
-                            <tr className='day-names'>
-                                <WeekDayNames />
-                            </tr>
-                        </thead>
-                        <tbody className='days-display-grid'>
-                            <tr className='grid-days'>
-                                <Week dayNumbers={this.state.dayArray.slice(0,7)} />
-                                <Week dayNumbers={this.state.dayArray.slice(7,14)} />
-                                <Week dayNumbers={this.state.dayArray.slice(14,21)} />
-                                <Week dayNumbers={this.state.dayArray.slice(21,28)} />
-                                <Week dayNumbers={this.state.dayArray.slice(28)} />
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+            <Fragment>
+                <button onClick={() => this.changeTheMonth('backward')}>Go Back A Month</button>
+                {this.state.relativeMoment ? <Month relativeMoment={this.state.relativeMoment}/> : null}
+                <button onClick={() => this.changeTheMonth('forward')}>Add a Month</button>
+            </Fragment>
         )
     }
 }
-
-export default Calendar
---
+++ calendar-app/src/frontend/lib/Month.jsx
@@ -0,0 +1,122 @@
+import React, { Component } from 'react'
+import '../styling/calendar.css'
+import Week from '../components/Week'
+import WeekDayNames from '../components/WeekDayNames'
+
+class Month extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            dayArray: []
+        }
+    }
+
+    loadTheDays(firstDay, dayArray, daysToFill, daysInLastMonth){
+        while (daysToFill > 0) {
+            dayArray.unshift(daysInLastMonth)
+            daysInLastMonth--
+            daysToFill--
+        }
+
+        let i = 1;
+        while (dayArray.length < 42) {
+            dayArray.push(i)
+            i++
+        }
+
+        return dayArray
+    }
+
+    divideUpDays(firstDay, daysInMonth, daysInLastMonth, dayArray){
+        let daysToFill;
+        
+        switch (firstDay) {
+            case 'Monday':
+                daysToFill = 0;
+                break;
+            case 'Teusday':
+                daysToFill = 1;
+                break;
+            case 'Wednesday':
+                daysToFill = 2;
+                break;
+            case 'Thursday':
+                daysToFill = 3;
+                break;
+            case 'Friday':
+                daysToFill = 4;
+                break;
+            case 'Saturday':
+                daysToFill = 5;
+                break;
+            case 'Sunday':
+                daysToFill = 6;
+                break;
+            default:
+                daysToFill = null;
+                break;
+        }
+        return this.loadTheDays(firstDay, dayArray, daysToFill, daysInLastMonth)
+    }
+
+    getDayArray(daysInMonth){
+        const arrDays = []
+
+        while (daysInMonth) {
+            const current = daysInMonth
+            arrDays.unshift(current)
+            daysInMonth--
+        }
+        return arrDays
+    }
+
+    getCalendarOutlook(){
+        debugger
+        const daysInMonth = this.props.relativeMoment.daysInMonth()
+        const daysInLastMonth = this.props.relativeMoment.subtract(1, 'month').daysInMonth()
+        const firstDay = this.props.relativeMoment.startOf('month').format('dddd')
+        const dayArray = this.getDayArray(daysInMonth)
+        this.divideUpDays(firstDay, daysInMonth, daysInLastMonth, dayArray)
+        this.setState({ dayArray })
+    }
+
+    componentDidUpdate(prevProps){
+        debugger
+        if (this.props.relativeMoment._d !== prevProps.relativeMoment._d) {
+            this.getCalendarOutlook()
+        }
+    }
+
+    componentDidMount() {
+        this.getCalendarOutlook()
+    }
+
+    render() {
+        return (
+            <div className="calendar-positioner">
+                <div className='calendar-container'>
+                    <table>
+                        <thead className='days-of-week'>
+                            <tr className='day-names'>
+                                <WeekDayNames />
+                            </tr>
+                        </thead>
+                        <tbody className='days-display-grid'>
+                            <tr className='grid-days'>
+                                <Week dayNumbers={this.state.dayArray.slice(0,7)} />
+                                <Week dayNumbers={this.state.dayArray.slice(7,14)} />
+                                <Week dayNumbers={this.state.dayArray.slice(14,21)} />
+                                <Week dayNumbers={this.state.dayArray.slice(21,28)} />
+                                <Week dayNumbers={this.state.dayArray.slice(28, 35)} />
+                                <Week dayNumbers={this.state.dayArray.slice(35)} />
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Month
